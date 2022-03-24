0bbbb2d0ab0976f270660b84529e8fe029f4cbc2
Amazida <102134676+zidaaja@users.noreply.github.com>
2022-03-24 09:35:47 +0800

    Update PRAK202.php

--- Modul2/PRAK202.php
+++ Modul2/PRAK202.php
@@ -1,10 +1,7 @@
 <?php
-    error_reporting(0);
-        if(isset($_POST['submit'])){
-            $nama=$_POST['nama'];
-            $nim=$_POST['nim'];
-            $gender=$_POST['gender'];
-        }
+    if(isset($_POST['submit'])){
+        $gender=$_POST['gender'];
+    }
 ?>
 
 <!DOCTYPE html>
@@ -18,14 +15,14 @@
             }
         </style>
         <form actions="" method="post">
-            Nama : <input type="text" name="nama" > <text>* 
+            Nama : <input type="text" name="nama" value=<?php if(isset($_POST['submit'])){$nama=$_POST['nama']; echo $nama;}?>> <text>* 
                 <?php if(isset($_POST['submit'])) if($nama == "") echo "Nama tidak boleh kosong"?>
             </text> <br>
-            NIM : <input type="text" name="nim" > <text>* 
+            NIM : <input type="text" name="nim" value=<?php if(isset($_POST['submit'])){$nim=$_POST['nim']; echo $nim;}?>> <text>* 
             <?php if(isset($_POST['submit'])) if($nim == "") echo "NIM tidak boleh kosong"?>
             </text> <br>
             Jenis Kelamin : <text>* 
-            <?php if(isset($_POST['submit'])) if($gender == "") echo "Jenis kelamin tidak boleh kosong"?>
+            <?php  if(isset($_POST['submit'])) if($gender == "") echo "Jenis kelamin tidak boleh kosong"?>
             </text> <br>
             <input type="radio" id="male" name="gender" value="Laki-Laki">
             <label for="male">Laki-laki</label><br>
@@ -35,6 +32,7 @@
         </form>
         
         <?php
+            error_reporting(0);
             if(($nama != "") and ($nim != "") and ($gender != "")){
                 echo $nama."</br>";
                 echo $nim."</br>";
